4c3f5b00bb7d52900bdee1207873bc01138e7968
KingletElvira <149202662+KingletElvira@users.noreply.github.com>
2025-06-05 00:17:15 +0300

    Header.jsx

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -3,8 +3,8 @@ export function Header() {
         <nav>
             <div className="nav-wrapper teal lighten-2 header">
                 <div className="container">
-                    <a href="#" class="brand-logo">React shop</a>
-                    <ul id="nav-mobile" class="right hide-on-med-and-down">
+                    <a href="#" className="brand-logo">React shop</a>
+                    <ul id="nav-mobile" className="right hide-on-med-and-down">
                         <li><a href="#!">Repository</a></li>
                     </ul>
                 </div>
